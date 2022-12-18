d4641be82b02a3489f31e77252105b142274fde5
frank <frank@Debian.myguest.virtualbox.org>
2022-12-18 18:54:01 +0100

    create shared library

--- src/Runtime.cpp
+++ src/Runtime.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char **argv) {
 	std::cout << "Hello World" << std::endl;
-	std::cout << "Version " << Runtime_VERSION_MAJOR << "." << Runtime_VERSION_MINOR << std::endl;
+	std::cout << "Version " << PLUGIN_VERSION_MAJOR << "." << PLUGIN_VERSION_MINOR << std::endl;
 
-	std::cout << "MAJOR_VERSION" << VERSION_MAJOR << std::endl;
+	std::cout << "MAJOR_VERSION" << PLUGIN_VERSION_MAJOR << std::endl;
 	return 0;
 }
