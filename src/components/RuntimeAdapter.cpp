f712451921d317c27ab0077590492f866709269d
frank <frank@Debian.myguest.virtualbox.org>
2022-12-24 11:28:01 +0100

    add parameter to subscribe, add variable runtimes, add method get

--- src/components/RuntimeAdapter.cpp
+++ src/components/RuntimeAdapter.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-RuntimeAdapter::RuntimeAdapter(std::string const &_type, std::string const &_name) : type(_type), name(_name)
+RuntimeAdapter::RuntimeAdapter( std::string const &_type, std::string const &_name ) : type( _type ), name( _name )
 {
 }
 
@@ -12,28 +12,42 @@ RuntimeAdapter::~RuntimeAdapter()
 
 std::string RuntimeAdapter::getType()
 {
-	return type;
+    return type;
 }
 
 std::string RuntimeAdapter::getName()
 {
-	return name;
+    return name;
 }
 
 std::string RuntimeAdapter::getVersion()
 {
-	return std::string("0.0.0.1");
+    return std::string( "0.0.0.1" );
 }
 
 void RuntimeAdapter::startApplications()
 {
-	std::cout << "RuntimeAdapter::startApplications" << std::endl;
+    std::cout << "RuntimeAdapter::startApplications" << std::endl;
 }
 
 void RuntimeAdapter::stopApplications()
 {
 }
 
-void RuntimeAdapter::subscribe()
+void RuntimeAdapter::subscribe( std::string const &name, std::shared_ptr< RuntimeIf > const runtime )
 {
+    runtimes[ name ] = runtime;
+}
+
+std::shared_ptr< RuntimeIf > RuntimeAdapter::get( std::string const &name ) const
+{
+    std::shared_ptr< RuntimeIf > result( nullptr );
+
+    auto const it = runtimes.find( name );
+    if ( it != runtimes.end() )
+    {
+        result = it->second;
+    }
+
+    return result;
 }
--- src/components/RuntimeAdapter.h
+++ src/components/RuntimeAdapter.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <map>
+#include <memory>
 #include <string>
+
 #include "runtime-sdk/RuntimeAdapterIf.h"
+#include "runtime-sdk/RuntimeIf.h"
 
 class RuntimeAdapter : public RuntimeAdapterIf
 {
@@ -15,10 +19,12 @@ class RuntimeAdapter : public RuntimeAdapterIf
 
     virtual void startApplications() override;
     virtual void stopApplications() override;
-    virtual void subscribe() override;
+    virtual void subscribe( std::string const &name, std::shared_ptr< RuntimeIf > const runtime ) override;
+    virtual std::shared_ptr< RuntimeIf > get( std::string const &name ) const override;
 
    private:
+    std::map< std::string, std::shared_ptr< RuntimeIf > > runtimes;
+
     std::string type;
     std::string name;
-
 };
