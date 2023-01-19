d376f9d2e3f9a79d0ebbdb895d498c474259136a
Frank Schumann <frank.schumann.76@gmail.com>
2023-01-19 19:04:37 +0100

    add googletest project as runtimeTest

---
+++ test/src/factory/RuntimeAdapterFactoryTest.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include "factory/RuntimeAdapterFactory.h"
+
+TEST( RuntimeAdapterFactoryTest, Failed )
+{
+    GTEST_FAIL();
+}
+
+TEST( RuntimeAdapterFactoryTest, create )
+{
+	RuntimeAdapterFactory runtimeAdapterFactory;
+
+    std::string const name = std::string( "Daisy Duck" );
+    auto runtimeAdapter = runtimeAdapterFactory.create( name );
+
+    ASSERT_NE( nullptr, runtimeAdapter );
+
+    ASSERT_EQ( name, runtimeAdapter->getName() );
+    ASSERT_EQ( std::string( "RuntimeController" ), runtimeAdapter->getType() );
+
+}
+
+TEST( RuntimeAdapterFactoryTest, getType )
+{
+    RuntimeAdapterFactory runtimeAdapterFactory;
+
+    std::string const type = runtimeAdapterFactory.getType();
+
+    EXPECT_EQ( "RuntimeAdapter", type );
+}
---
+++ test/src/factory/RuntimeControllerFactoryTest.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include "factory/RuntimeControllerFactory.h"
+
+TEST( RuntimeControllerFactoryTest, Failed )
+{
+    GTEST_FAIL();
+}
+
+TEST( RuntimeControllerFactoryTest, create )
+{
+    RuntimeControllerFactory runtimeControllerFactory;
+
+    std::string const name = std::string( "Daisy Duck" );
+    auto runtimeController = runtimeControllerFactory.create( name );
+
+    ASSERT_NE( nullptr, runtimeController );
+
+    ASSERT_EQ( name, runtimeController->getName() );
+    ASSERT_EQ( std::string( "RuntimeController" ), runtimeController->getType() );
+
+}
+
+TEST( RuntimeControllerFactoryTest, getType )
+{
+    RuntimeControllerFactory runtimeControllerFactory;
+
+    std::string const type = runtimeControllerFactory.getType();
+
+    EXPECT_EQ( "RuntimeController", type );
+}
