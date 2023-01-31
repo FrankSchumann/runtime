08131f976e95ed1f1f046c8548b49e28bc4f2d2a
Frank Schumann <frank.schumann.76@gmail.com>
2023-01-31 22:58:49 +0100

    add conan recipe, test_package not working yet because example.exe needs copa references

---
+++ none_test_package/src/example.cpp
@@ -0,0 +1,18 @@
+#include "runtime/RuntimeIf.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hallo Welt!" << std::endl;
+}
+
+namespace COPA
+{
+class ComponentController
+{
+    ComponentController(){}
+};
+
+
+}
