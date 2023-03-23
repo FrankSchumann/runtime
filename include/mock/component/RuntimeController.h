0646f12fe3febbd7f8c821ac3c6844eff41c6705
Frank Schumann <frank.schumann.76@gmail.com>
2023-03-24 00:33:22 +0100

    fix gtest

--- include/mock/component/RuntimeController.h
+++ include/mock/component/RuntimeController.h
@@ -11,10 +11,12 @@ class RuntimeController : public RuntimeControllerIf
     {
     }
 
-    MOCK_METHOD( void, subscribe, ( std::string const& name, std::shared_ptr< RuntimeIf > const runtime ), ( override ) );
+    MOCK_METHOD( void, subscribe, ( std::shared_ptr< RuntimeIf > const runtime ), ( override ) );
+    MOCK_METHOD( void, unsubscribe, ( std::string const& name ), ( override ) );
     MOCK_METHOD( std::shared_ptr< RuntimeIf >, get, ( std::string const& name ), ( const, override ) );
     MOCK_METHOD( ( std::map< std::string, std::shared_ptr< RuntimeIf > > ), getAll, (), ( const, override ) );
 
+    // core::ComponentIf
     MOCK_METHOD( std::string, getType, (), ( const, override ) );
     MOCK_METHOD( std::string, getName, (), ( const, override ) );
     MOCK_METHOD( std::string, getVersion, (), ( const, override ) );
